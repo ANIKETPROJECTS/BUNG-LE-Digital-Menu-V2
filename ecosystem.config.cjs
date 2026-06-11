7c0980098dc4ace7e0006fac142f3ec8262c4133
ANIKETPROJECTS <aniketraneworkemail@gmail.com>
2026-06-11 10:45:13 +0530

    Rename app and update environment variables

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   apps: [{
-    name: "Tarangdigitalmenu",
+    name: "BungleDigitalMenu",
     script: "./dist/index.js",
     instances: 1,
     autorestart: true,
@@ -8,8 +8,8 @@ module.exports = {
     max_memory_restart: "1G",
     env: {
       NODE_ENV: "production",
-      PORT: 3010,
-      MONGODB_URI: "mongodb+srv://raneaniket23_db_user:OiUJaUQdc1FsXmzn@atdigitalmenu.prd9tyh.mongodb.net/?appName=ATDIGITALMENU",
+      PORT: 4010,
+      MONGODB_URI: "mongodb+srv://raneaniket23_db_user:5MIiySrJlljOOOBY@bungle.hggnj83.mongodb.net/?appName=BUNGLE",
       SESSION_SECRET: "ZTYj/a2OXy7iabAxcXL+ue17Aw3pQPg7wmyjLWvduDU4t/jS37xQriltz3khKboxGBhjje3+rqc7l7YuNmdOCA=="
     }
   }]
